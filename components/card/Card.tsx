00ff28defd4951af32fa60d9853deb932a6fd476
Melvin Menanteau <melvin.menanteau@gmail.com>
2022-11-27 16:53:17 +0100

    Test

---
+++ components/card/Card.tsx
@@ -0,0 +1,57 @@
+import { CardProps } from "../../types/Card.interface";
+import Pike from "../../components/svg/pike";
+import Tile from "../../components/svg/tile";
+import Clover from "../../components/svg/clover";
+import Heart from "../../components/svg/heart";
+
+import styles from '../../assets/css/Card.module.css';
+
+const Card = (CardProps : CardProps) => {
+    const getDisplayNumber = () : string => {
+        switch (CardProps.number) {
+            case 1:
+                return 'A';
+            case 11:
+                return 'J';
+            case 12:
+                return 'Q';
+            case 13:
+                return 'K';
+            default:
+                return (CardProps.number).toString();
+        }
+    };
+
+    const dragEnd = (event : any) => {
+        // console.log(CardProps);
+        console.log('Card: dragEnd');
+        console.log(CardProps);
+    }
+
+    const drop = (event : any) => {
+        // console.log(CardProps);
+        console.log('Card: drop');
+        console.log(CardProps);
+    }
+
+    return (
+        <div
+            className={`${styles.Card} Card`}
+            onDragOver={(event) => event.preventDefault()}
+            draggable
+        >
+            <div>
+                <div className={`${styles.CardNumber} top`} style={{color: CardProps.color === 'red' ? '#EF5050' : '#2F3A58'}}>{getDisplayNumber()}</div>
+                <div className={styles.CardSymbol}>{
+                    CardProps.symbol === 'pike' ? <Pike /> : 
+                        CardProps.symbol === 'tile' ? <Tile /> :
+                            CardProps.symbol === 'clover' ? <Clover /> :
+                                CardProps.symbol === 'heart' ? <Heart /> : null
+                }</div>
+                <div className={`${styles.CardNumber} ${styles.bottom}`} style={{color: CardProps.color === 'red' ? '#EF5050' : '#2F3A58'}}>{getDisplayNumber()}</div>
+            </div>
+        </div>
+    )
+};
+
+export default Card;
---
+++ types/Card.interface.ts
@@ -0,0 +1,13 @@
+export interface CardProps {
+    // color: "red" | "black",
+    color: string,
+    /*
+        heart: coeur
+        tile: carreau
+        clover: trèfle
+        pike: pique
+    */
+    // symbol: "heart" | "tile" | "clover" | "pike",
+    symbol: string,
+    number: number
+}
